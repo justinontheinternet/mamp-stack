75b4939872685eb49f63aa5f625535ec9a083da4
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-08-22 19:26:31 -0400

    headers and redirects

---
+++ headers.php
@@ -0,0 +1,18 @@
+<?php
+  header("HTTP 1.1/ 404 Not Found");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Headers</title>
+</head>
+<body>
+
+<?php
+  // This is too late...unless you have output buffering turned on.
+  // header("HTTP 1.1/ 404 Not Found");
+?>
+
+</body>
+</html>
---
+++ redirect.php
@@ -0,0 +1,16 @@
+<?php
+  // This is how you redirect
+  header("Location: first_page.php");
+  // stop loading anything else
+  exit;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+
+</body>
+</html>
