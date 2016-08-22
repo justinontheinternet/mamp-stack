0cf23886fc6d8c1ae9bb0489e1c437982a2f4d0d
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-08-22 17:28:41 -0400

    initial commit. Links.

---
+++ first_page.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>First Page</title>
+</head>
+<body>
+  <a href="second_page.php">Second Page</a>
+</body>
+</html>
---
+++ second_page.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Second Page</title>
+</head>
+<body>
+<a href="first_page.php">First Page</a>
+</body>
+</html>
