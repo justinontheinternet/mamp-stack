90f2b3c75725218be533790eb834ed02d34e7e7f
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-08-22 17:52:15 -0400

    url params

--- first_page.php
+++ first_page.php
@@ -4,6 +4,8 @@
   <title>First Page</title>
 </head>
 <body>
-  <a href="second_page.php">Second Page</a>
+  <?php $link_name = "Second Page"; ?>
+  <?php $id = 2; ?>
+  <a href="second_page.php?id=<?php echo $id; ?>"><?php echo $link_name ?></a>
 </body>
 </html>
--- second_page.php
+++ second_page.php
@@ -4,6 +4,14 @@
   <title>Second Page</title>
 </head>
 <body>
-<a href="first_page.php">First Page</a>
+  <a href="first_page.php">First Page</a>
+
+  <pre>
+    <?php 
+      // print_r($_GET);
+      $id = $_GET['id'];
+      echo $id;
+    ?>
+  </pre>
 </body>
 </html>
