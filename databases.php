32bbc295f7bd5dbb592303d934ea0d31d3fcf7dc
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-08-23 10:53:16 -0400

    fetching data with a query

--- databases.php
+++ databases.php
@@ -10,6 +10,21 @@
     die( "Database connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")" );
   }
 ?>
+<?php
+  // 2. Perform database query
+    // assemble query like below for readability and to incorporate conditionals
+    // make sure there is a space at the end of each string
+  $query = "SELECT * ";
+  $query .= "FROM subjects ";
+  $query .= "WHERE visible = 1 ";
+  $query .= "ORDER BY position ASC";
+  // $result is a 'resource': a collection of db rows
+  $result = mysqli_query($connection, $query);
+  // Test if there was query error. Empty result is not the same as no result.
+  if (!$result) {
+    die("Database query failed.");
+  }
+?>
 
 <!DOCTYPE html>
 <html>
@@ -17,6 +32,23 @@
   <title>Databases</title>
 </head>
 <body>
+
+  <?php
+    // 3. Use return data (if any)
+      // fetching row increments array pointer for us
+      // can't use foreach() because $result is not a php object, it is a mysql result set
+      // saying "while you can still assign something to $row, dump $row, otherwise exit"
+    while ($row = mysqli_fetch_row($result)) {
+      // output data from each row
+      var_dump($row);
+      echo "<hr />";
+    }
+  ?>
+
+  <?php
+    // 4. Release returned data to free memory
+    mysqli_free_result($result);
+  ?>
 
 </body>
 </html>
